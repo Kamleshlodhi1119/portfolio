a5b3a9d15134826a30851456bc0834b0325e1a84
KAMLESH LODHI <kamleshlodhi9302@gmail.com>
2023-12-22 11:15:26 +0530

    commit

--- main.js
+++
@@ -1,9 +0,0 @@
-// This JavaScript code will change the color of the header when the user scrolls down the page.
-
-window.onscroll = function() {
-    if (window.scrollY > 200) {
-        document.querySelector("header").style.backgroundColor = "#fff";
-    } else {
-        document.querySelector("header").style.backgroundColor = "#000";
-    }
-};
